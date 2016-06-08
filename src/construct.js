fa5162792ad9fb0afe0a60c1fbb62dcf3e8ab990
Erika Pauwels <erika.pauwels@gmail.com>
2016-06-08 22:52:51 +0200

    Use prefixes in SPARQL query

--- src/construct.js
+++ src/construct.js
@@ -5,16 +5,17 @@ export default [
   {
     path: '/pipelines',
     query: `
-      PREFIX core: <http://mu.semte.ch/vocabularies/core/>
+      PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+      PREFIX pwo: <http://purl.org/spar/pwo/>
       CONSTRUCT {
         ?pipeline ?p ?o .
         ?step ?x ?y
       }
       FROM <${graph}>
       WHERE {
-        ?pipeline core:uuid ?uuid .
+        ?pipeline mu:uuid ?uuid .
         ?pipeline ?p ?o .
-        ?pipeline <http://purl.org/spar/pwo/hasStep> ?step .
+        ?pipeline pwo:hasStep ?step .
         ?step ?x ?y
       }`,
     config: {
